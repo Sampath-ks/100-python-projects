e282778ce3ad29221c671d5525c57ea88448f739
Sampath-ks <sampathchokkady@gmail.com>
2025-04-26 12:10:36 +0530

    Add multiplication generator

---
+++ 08-multiplication_generator/08-Multiplication_generator.py
@@ -0,0 +1,8 @@
+  
+a=int(input("Enter the number:"))
+b=int(input("Enter the range:"))
+print(f"Multiplication Table of {a} \n---------------------------")
+   
+for i in range(0,b+1):
+    if b>=b:
+        print(f"{a} * {i} =",a*i)
